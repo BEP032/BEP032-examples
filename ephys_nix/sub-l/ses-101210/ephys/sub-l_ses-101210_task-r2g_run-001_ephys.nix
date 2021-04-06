4ec2a070abd85c6ba5aacc190bb9a78329a46390
sprenger <julia.sprenger@univ-amu.fr>
2021-04-06 11:23:59 +0200

    gin commit from nit-ope-de04

--- ephys_nix/sub-l/ses-101210/ephys/sub-l_ses-101210_task-r2g_run-001_ephys.nix
+++ ephys_nix/sub-l/ses-101210/ephys/sub-l_ses-101210_task-r2g_run-001_ephys.nix
@@ -1,1 +1,1 @@
-/annex/objects/MD5E-s36120562--6685ed96f25374c596bbbb7b80b4a69f.nix
+../../../../.git/annex/objects/x5/Zf/MD5E-s36120562--6685ed96f25374c596bbbb7b80b4a69f.nix/MD5E-s36120562--6685ed96f25374c596bbbb7b80b4a69f.nix
